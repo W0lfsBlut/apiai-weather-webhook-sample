31b6616985cc786107986327e8b5cccacddcc4f1
W0lfsBlut <cneumeier@live.de>
2016-11-23 15:08:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -86,8 +86,8 @@ def makeWebhookResult(data):
     return {
         "speech": speech,
         "displayText": speech,
-        # "data": data,
-        # "contextOut": [],
+        "data": data,
+        "contextOut": [],
         "source": "apiai-weather-webhook-sample"
     }
 
